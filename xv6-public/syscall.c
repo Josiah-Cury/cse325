3ea38155168dba806b0e75767064099e12b94b54
Josiah Cury <josiah.cury@student.nmt.edu>
2021-11-06 18:01:17 -0600

    Copied fork to thread_create

--- xv6-public/syscall.c
+++ xv6-public/syscall.c
@@ -129,6 +129,9 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+[SYS_thread_create]		sys_thread_create,
+[SYS_thread_join]		sys_thread_join,
+[SYS_thread_exit]		sys_thread_exit,
 };
 
 void
